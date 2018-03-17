ac038b8679c706240c40bb4cd251ddc7ddf15ab3
Keily Salinas <keisalinas2@gmail.com>
2018-03-17 10:46:02 -0400

    added media component

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
+import Media from './src/playlist/components/media';
 
 const app = document.getElementById('app')
 
 // ReactDOM.render(render element, where);
-const holaMundo = <h1>hello world</h1>;
-ReactDOM.render(holaMundo , app);
+render(<Media /> , app);
 
