ca30427bb156328ec8be1c6ce1d267cb42191839
Keily Salinas <keisalinas2@gmail.com>
2018-03-17 00:19:00 -0400

    use react-dom

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log('Hola mundo!')
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const app = document.getElementById('app')
+
+// ReactDOM.render(render element, where);
+const holaMundo = <h1>hello world</h1>;
+ReactDOM.render(holaMundo , app);
+
