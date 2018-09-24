de274937c9038eba04d6e55aaabc6856f7b802d5
Keily Salinas <keisalinas2@gmail.com>
2018-09-23 22:41:19 -0500

    handle video duration and time

---
+++ src/player/components/timer.js
@@ -0,0 +1,20 @@
+
+import React from 'react';
+import {
+  formattedTime
+} from '../../lib/utils';
+import './timer.css';
+
+function Timer(props) {
+  const currentTime = formattedTime(props.currentTime)
+  const duration = formattedTime(props.duration)
+  return (
+    <div className="Timer">
+      <p>
+        <span>{currentTime} / {duration}</span>
+      </p>
+    </div>
+  )
+}
+
+export default Timer;
