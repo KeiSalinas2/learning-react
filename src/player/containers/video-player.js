d172dc8e6b8b0056e21f47a4c380ed90149c4e9d
Keily Salinas <keisalinas2@gmail.com>
2018-09-23 22:22:30 -0500

    handle video play and pause events

--- src/player/containers/video-player.js
+++ src/player/containers/video-player.js
@@ -2,16 +2,35 @@ import React, { Component } from 'react';
 import VideoPlayerLayout from '../components/video-player-layout';
 import Video from '../components/video';
 import Title from '../components/title';
+import PlayPause from '../components/play-pause';
 
 class VideoPlayer extends Component {
+  state = {
+    pause: true,
+  }
+  togglePlay = (event) => {
+    this.setState(prevState => ({
+      pause: !prevState.pause
+    }))
+  }
+  componentDidMount() {
+    this.setState({
+      pause: (!this.props.autoplay)
+    })
+  }
   render() {
     return (
       <VideoPlayerLayout>
         <Title
           title="Esto es un video chido!"
         />
+        <PlayPause
+          pause={this.state.pause}
+          handleClick={this.togglePlay}
+        />
         <Video
-          autoplay={true}
+          autoplay={this.props.autoplay}
+          pause={this.state.pause}
           src="http://download.blender.org/peach/bigbuckbunny_movies/BigBuckBunny_320x180.mp4"
         />
       </VideoPlayerLayout>
@@ -19,4 +38,4 @@ class VideoPlayer extends Component {
   }
 }
 
-export default VideoPlayer;
+export default VideoPlayer
