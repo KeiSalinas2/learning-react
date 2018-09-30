9cfa81ce19f110c19983fdb3312255a368afd2a1
Keily Salinas <keisalinas2@gmail.com>
2018-09-30 14:11:49 -0500

    adding middlewares

--- app/src/entries/home.js
+++ app/src/entries/home.js
@@ -2,18 +2,23 @@ import React from 'react';
 import { render } from 'react-dom';
 import Home from '../pages/containers/home';
 
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import { Map as map } from 'immutable';
+import logger from 'redux-logger';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 import reducer from '../../redux/reducers/';
 import data from '../../schemas/';
 
-
 const store = createStore(
   reducer,
   map(),
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  composeWithDevTools(
+    applyMiddleware(
+      logger
+    )
+  )
 );
 
 const app = document.getElementById('home-container')
